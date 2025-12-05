5bc3a8152c79321b0d297ad0c5b9cd8fd7898189
Andrew-F09 <fionxia11@gmail.com>
2025-12-05 14:23:15 -0500

    my world format created

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,29 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    
+    public int score = 0;
+  
+    int level = 1;
+    
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
+        
+        createApple();
+    }
+    
+    public void gameOver() {
+        
+    }
+    
+    public void increaseScore() {
+    
+        }
+    
+    public void createApple() {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
