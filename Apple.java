8a3091f296a5c6fa43ed0524679ae8af65492de5
Andrew-F09 <fionxia11@gmail.com>
2025-12-05 14:30:03 -0500

    apple mechanics added

--- Apple.java
+++ Apple.java
@@ -15,6 +15,11 @@ public class Apple extends Actor
     public void act()
     {
         setLocation(getX(), getY() + 3);
-        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight()) {
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
 }
+
